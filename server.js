f1adc4414f7cc8765cefefe930d9bd05b9912be4
wietraum <80662027+wietraum0301@users.noreply.github.com>
2025-09-10 14:18:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// 정적 파일은 public 폴더에서 제공 (public 폴더 생성 필요)
-app.use(express.static(path.join(__dirname, 'public')));
+// 현재 폴더(루트)에서 정적 파일 제공
+app.use(express.static('.'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// 에러 핸들링 예시
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('서버 오류가 발생했습니다.');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
